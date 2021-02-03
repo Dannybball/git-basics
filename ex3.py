ed0df8d9d55d49eeb4b97518a10ec66971cfb0e2
Dannybball <51676611+Dannybball@users.noreply.github.com>
2021-02-03 15:28:50 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,47 @@
+print("i will now count my chickens:")
+
+print("Hens", 25+30/6)
+print("Hens", float(25+35/6))
+
+#BODMAS
+print("Roosters", 100-25*3 %4)
+
+print("Now I will count the eggs: ")
+
+print(3+2+1-5+4%2-1/4+6)
+# Output: 6.75 
+
+print("is it true that 3+2<5-7?")
+
+#use of inequalities
+print(3+2<5-7)
+# Output: False
+
+print("what is 3+2?", 3+2)
+print("what is 5-7?", 5-7)
+
+
+print("oh, that's why it's False.")
+print("How about some more. ")
+
+print("Is it greater?", 5>-2)
+# Output: True
+print("Is it greater or equal ?", 5>=-2)
+# Output: True
+print("Is it less or equal ?", 5<=-2)
+# Output: False
+
+print("Is it not equal ?", 5!=5)
+# Output: False
+
+# b = "Hello there"
+# print(len(b))
+# # Output: 11
+
+while True:
+    print("Please type your name. >")
+    name = input()
+    if name =="your name":
+        break
+
+print("Thank you")
